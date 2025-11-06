f6bb41c163acc0b486ee64d6803299f519aa94da
toxicpapa00 <gyaneshpatel605@gmail.com>
2025-11-06 09:38:48 +0530

    Update help.py

--- Clonify/cplugin/help.py
+++ Clonify/cplugin/help.py
@@ -107,7 +107,7 @@ async def helper_cb(client, CallbackQuery, _):
     if cb == "hb9":
         if CallbackQuery.from_user.id not in SUDOERS:
             return await CallbackQuery.answer(
-                "😎 Pehle Zeo Ko Papa Bol Jake 😆😆", show_alert=True
+                "😎 Pehle Toxic Ko Papa Bol Jake 😆😆", show_alert=True
             )
         else:
             await CallbackQuery.edit_message_text(helpers.HELP_9, reply_markup=keyboard)
