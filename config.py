b74be4e1a5a579797d693ff5e4a4b61a8386074b
toxicpapa00 <gyaneshpatel605@gmail.com>
2025-11-06 10:22:36 +0530

    Update config.py

--- config.py
+++ config.py
@@ -34,7 +34,7 @@ HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 SOURCE = getenv("SOURCE", "https://files.catbox.moe/uyfhoe.mp4")
-UPSTREAM_REPO = getenv("UPSTREAM_REPO", "t.me/ll_TOXICC_PAPA_ll")
+UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/toxicpapa00/clonemusic")
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv("GIT_TOKEN", None)
 
